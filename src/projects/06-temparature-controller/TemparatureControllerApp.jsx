d304ec5007e34dcf04943b7a88c475810470f7d4
muratozbk <slipstream11@yandex.com>
2023-06-29 12:34:32 +0300

    change temparature

--- src/projects/06-temparature-controller/TemparatureControllerApp.jsx
+++ src/projects/06-temparature-controller/TemparatureControllerApp.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 import Button from '../components/Button'
 
 export default function TemparatureControllerApp() {
+    const [temparature, setTemparature] = useState(0)
+
     return (
         <div className='container mt-3 text-center'>
             <div className="card m-auto"
                 style={{ width: '250px' }}>
-                <h1 className='card m-auto'
-                    style={{ height: '150px', width: '150px', borderRadius: '50%' }}>Temparature</h1>
+                <div className={`card m-auto d-flex
+                 ${temparature > 0 ? 'bg-danger' : 'bg-info'}`}
+                    style={{ height: '150px', width: '150px', borderRadius: '50%' }}>
+                    <h1 style={{
+                        fontSize: '2.6rem'
+                    }}> {temparature}° C</h1>
+                </div>
                 <div className="d-flex"
                     style={{ margin: '2rem 0' }}>
                     <Button text='-' btnClass={'btn-lg mr-1'}
-                        onClick={() => console.log('-')} />
+                        onClick={() => setTemparature(temparature - 1)} />
                     <Button text='+' btnClass={'btn-lg'}
-                        onClick={() => console.log('+')} />
+                        onClick={() => setTemparature(temparature + 1)} />
                 </div>
             </div>
         </div>
