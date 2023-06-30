c9c028fb45d8bd585335786d42b1b6f7e8f45b29
muratozbk <slipstream11@yandex.com>
2023-06-30 15:50:50 +0300

    unlock slider

--- src/projects/08-slide-to-unlock/LockSlider.jsx
+++ src/projects/08-slide-to-unlock/LockSlider.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./LockSlider.css"
 
-export default function LockSlider({ width, handleInput, sliderValue }) {
+export default function LockSlider({ showLock, width, handleInput, sliderValue }) {
     let sliderStyle = {
         appearance: 'none',
         width: !width ? '300px' : width,
         height: '50px',
         backgroundColor: 'rgba(188, 190, 188, 0.5)',
         ooutline: 'none',
-        borderRadius: '5px',
-
+        borderRadius: '25px',
     }
+
 
     return (
         <input type="range" className='slider mb-2'
--- src/projects/08-slide-to-unlock/SlideToUnlock.jsx
+++ src/projects/08-slide-to-unlock/SlideToUnlock.jsx
@@ -1,28 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import LockSlider from './LockSlider'
 import { AiFillUnlock } from 'react-icons/ai'
-import LockSreenImg from './img/moon.jpg'
+import LockScreenImg from './img/moon.jpg'
 import HomeScreenImg from './img/home.jpg'
 
 export default function SlideToUnlock() {
     const [uiProps, setUiProps] = useState({
         uiText: 'Unlock screen',
         uiColor: '#eee',
-        uiBg: `url(${LockSreenImg}) center/cover`
+        uiBg: `url(${LockScreenImg}) center/cover`
     })
     const [showLock, setShowLock] = useState(true)
+    const [lockSliderValue, setLockSliderValue] = useState(0)
+
+    const handleSliderInput = (e) => {
+        setLockSliderValue(e.target.value)
+        // setShowLock(false)
+    }
+
+    const resetSlider = () => {
+        setLockSliderValue(0);
+    };
+
+    useEffect(() => {
+        if (lockSliderValue > 99) {
+            setShowLock(false)
+            setUiProps(prevState => ({
+                ...prevState,
+                uiBg: `url(${HomeScreenImg}) center/cover`
+            }))
+            resetSlider()
+        }
+
+    }, [lockSliderValue])
+
+    const handleClick = () => {
+        setLockSliderValue(0)
+        setUiProps(prevState => ({
+            ...prevState,
+            uiBg: `url(${LockScreenImg}) center/cover`
+        }))
+        setShowLock(true)
+    }
+
     return (
         <div style={{
             height: '80vh', marginTop: '9vh', width: '340px',
             border: '4px solid #000', borderRadius: '20px',
             background: uiProps.uiBg,
-            backgroundSize: 'cover'
         }}
             className="container text-center d-flex flex-column">
             <h1 className='title'
                 style={{ color: uiProps.uiColor }}>{uiProps.uiText}</h1>
-            {showLock ? <LockSlider width={'264px'} />
-                : <AiFillUnlock className='unlockIcon' />}
+            {showLock ?
+                <LockSlider handleInput={handleSliderInput}
+                    showLock={showLock} value={lockSliderValue} width={'264px'} />
+                : <AiFillUnlock onClick={handleClick} className='unlockIcon' />}
 
 
         </div>
