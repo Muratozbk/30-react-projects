e580205c52389f87969207a1083834d7b2b510a0
muratozbk <slipstream11@yandex.com>
2023-07-04 11:28:38 +0300

    search filter

--- src/projects/14-filter-contacts/FilterContacts.jsx
+++ src/projects/14-filter-contacts/FilterContacts.jsx
@@ -11,19 +11,30 @@ export default function FilterContacts() {
         inputSearch.current.focus()
     })
 
+    const [searchContact, setSearchContact] = useState('')
+
     return (
         <div className='text-center'>
             <Title text={'Filter Contacts App'} />
             <input type="text" placeholder='Search by first name'
                 ref={inputSearch}
+                onChange={e => setSearchContact(e.target.value)}
                 className='mb-2' style={{ padding: '.3rem .5rem' }} />
             <section className='d-flex' style={{
                 gap: 15,
-                maxWidth: 1600, margin: 'auto'
+                maxWidth: '1600px', margin: 'auto',
+                flexWrap: 'wrap'
             }}>
 
                 {
-                    ContactsData.map((contact) => (
+                    ContactsData.filter((contact) => {
+                        if (searchContact.trim() === '') {
+                            return contact;
+                        } else if (contact.first_name.toLowerCase()
+                            .includes(searchContact.trim().toLocaleLowerCase())) {
+                            return contact;
+                        }
+                    }).map((contact) => (
                         <Contact contact={contact} key={contact.id} />
                     ))
                 }
