880c7de345e20712d9bd916951092a4404f7bf9e
muratozbk <slipstream11@yandex.com>
2023-07-05 12:28:44 +0300

    create new note

--- src/projects/16-notes-app/NewNote.jsx
+++ src/projects/16-notes-app/NewNote.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { NoteStyle, Icons, DeleteIcon } from './NotesStyle'
+import { MdOutlineNoteAlt, MdDeleteForever } from 'react-icons/md'
 
-export default function NewNote() {
+export default function NewNote({ cardBg, note, onClick, text }) {
+    const [newCardBg, setNewCardbg] = useState();
+
+    useEffect(() => {
+        !newCardBg && setNewCardbg(cardBg)
+    }, [newCardBg])
+
     return (
-        <div>NewNote</div>
+        <NoteStyle className={`card ${newCardBg}`}>
+
+            <div className="card-header text-right">
+                <Icons>
+                    <MdOutlineNoteAlt />
+                    <DeleteIcon>
+                        <MdDeleteForever onClick={() => onClick(note.id)} />
+                    </DeleteIcon>
+                </Icons>
+            </div>
+            <div className="card-body">
+                <textarea style={{ border: 'none', outline: 'none' }}>
+                    {text}
+                </textarea>
+            </div>
+        </NoteStyle>
     )
 }
--- src/projects/16-notes-app/NotesApp.jsx
+++ src/projects/16-notes-app/NotesApp.jsx
@@ -1,16 +1,47 @@
 import { useState } from 'react'
 import NewNote from './NewNote'
 import Button from '../components/Button'
+import { v4 as uuidv4 } from 'uuid'
+
 
 export default function NotesApp() {
+    const [notes, setNotes] = useState([]);
+
+    const addNewNote = (newNote) => {
+        const newNotes = [...notes, newNote];
+        newNotes.filter((note) => !note.id && (note.id = uuidv4()))
+        setNotes(newNotes)
+    }
+
+    const deleteNote = (id) => {
+        setNotes(notes.filter(note => note.id !== id))
+    }
+
+    const cards = [
+        'card-info',
+        'card-primary',
+        'card-danger',
+        'card-success',
+        'card-warning'
+    ]
+
+    const randomizeBg = () => {
+        const random = Math.floor(Math.random() * cards.length);
+        return cards[random]
+    }
+
     return (
         <div className='container'>
-            <Button btnClass={'btn-info'} text='+ New note' />
+            <Button btnClass={'btn-info'}
+                text='+ New note' onClick={addNewNote} />
 
-            <div className="container d-flex"
+            <div className="container container-sm d-flex"
                 style={{ flexWrap: 'wrap', gap: 25 }}>
 
-                <NewNote />
+                {notes.map((note) => (
+                    <NewNote key={note.id} note={note} onClick={deleteNote}
+                        cardBg={randomizeBg()} />
+                ))}
             </div>
         </div>
     )
