b0287167c735d380c89f4840707c1abdd146bbad
muratozbk <slipstream11@yandex.com>
2023-07-07 11:27:54 +0300

    refocus

--- src/projects/18-text-animation/TextAnimationApp.jsx
+++ src/projects/18-text-animation/TextAnimationApp.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Button from '../components/Button'
 import FormGroup from '../components/FormGroup';
 import AnimatedText from 'react-animated-text-content'
@@ -16,14 +16,22 @@ export default function TextAnimationApp() {
         setInputValue(() => '')
     }
 
+    let InputText = useRef()
+
+    useEffect(() => {
+        InputText.current.focus()
+    })
+
     return (
         <div className='container text-center'>
             <form action="" className="container d-flex mt-4 mb-2"
                 onSubmit={(e) => e.preventDefault()}>
                 <FormGroup inputType={'text'}
                     onChange={handleInputChange}
-                    placeholder={'Your Text'} values={inputValue}
-                    labelText={'Type in your text to be animated'} />
+                    placeholder={'Your Text'}
+                    values={inputValue}
+                    labelText={'Type in your text to be animated'}
+                    reference={InputText} />
                 <Button text='Clear' btnClass={'btn-large btn-danger mt-3'}
                     onClick={handleClear} />
             </form >
--- src/projects/components/FormGroup.jsx
+++ src/projects/components/FormGroup.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-export default function FormGroup({ values,
+export default function FormGroup({
+    values,
     labelText,
     inputType,
     placeholder,
     onInput,
     onKeyUp, onChange,
     className,
-    readOnly = false
+    readOnly = false,
+    reference
 }) {
     return (
         <div className="form-group" >
             <label style={{ fontWeight: 'bold', fontSize: '1rem', padding: '.5rem', marginBottom: '1rem' }}>{labelText} </label>
             <input type={inputType} placeholder={placeholder}
                 value={values} onInput={onInput}
+                ref={reference}
                 className={className} readOnly={readOnly}
                 onKeyUp={onKeyUp} onChange={onChange} />
         </div>
