3f7947d902542747216044565b9a09d40f8e0d65
muratozbk <slipstream11@yandex.com>
2023-07-09 12:43:55 +0300

    add delete item

--- src/projects/21-task-tracker/TaskTrackerApp.jsx
+++ src/projects/21-task-tracker/TaskTrackerApp.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import Title from '../components/Title'
-import { TableStyle, NewTaskStyle } from './TableStyle'
+import { TableStyle } from './TableStyle'
 import Task from './Task'
+import NewTask from './NewTask'
 
 export default function TaskTrackerApp() {
     document.body.style.background = '#ddd'
@@ -18,6 +19,25 @@ export default function TaskTrackerApp() {
         setNewTask({ ...newTask, type: e.target.value })
     }
 
+    let tasks = [{ date: '', type: '' }];
+
+    const [taskList, setTaskList] = useState(tasks)
+
+    const addNewTask = () => {
+        setTaskList([...taskList, {
+            date: newTask.date,
+            type: newTask.type
+        }])
+    }
+
+    const handleCompleted = e => {
+        e.target.classList.toggle('completed')
+    }
+
+    const handleDelteTask = (e) => {
+        window.confirm('Delete this Task?') && e.target.parentElement.remove();
+    }
+
     return (
         <>
             <Title text={'Tasks Tracker App'} classes={'text-center mt-1 mb-2'} />
@@ -30,12 +50,18 @@ export default function TaskTrackerApp() {
                     date={newTask.date}
                     setDate={handleChangeDate}
                     type={newTask.type}
-                    setType={handleType} />
+                    setType={handleType}
+                    onClick={addNewTask} />
                 <ul className="table-row">
-                    <li className='completed'>Task</li>
-                    <li>Task</li>
-                    <li>Task</li>
-                    <li>Task</li>
+                    {taskList.map((taskItem, idx) =>
+                        taskItem.date !== '' && taskItem.type !== ''
+                            ? <NewTask key={idx}
+                                date={taskItem.date}
+                                type={taskItem.type}
+                                onTaskClick={handleCompleted}
+                                onDelete={handleDelteTask} /> : null
+                    )}
+
                 </ul>
             </TableStyle>
         </>
