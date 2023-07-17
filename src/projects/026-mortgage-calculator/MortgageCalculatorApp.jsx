ac56c1a57d14904a37d74b3d2cdbcd3409796405
muratozbk <slipstream11@yandex.com>
2023-07-17 14:24:55 +0300

    add states

--- src/projects/026-mortgage-calculator/MortgageCalculatorApp.jsx
+++ src/projects/026-mortgage-calculator/MortgageCalculatorApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import Title from '../components/Title'
 import FormGroup from '../components/FormGroup'
 import Button from '../components/Button'
@@ -7,10 +7,18 @@ import Button from '../components/Button'
 export default function MortgageCalculatorApp() {
     document.body.style.background = '#ddd';
     let alertClass;
+
+    const [homeValue, setHomeValue] = useState('');
+    const [downPayment, setDownPayment] = useState("");
+    const [loanAmount, setLoanAmount] = useState("")
+    const [interestRate, setInterestRate] = useState('')
+    const [loanDuration, setLoanDuration] = useState('')
+    const [monthlyPayment, setMonthlyPayment] = useState('')
+
     return (
         <div className='container mt-4 card'
             style={{ width: '500px' }}>
-            <Title text={'Mortgage Calc App'} />
+            <Title text={'Mortgage Calc App'} classes={'subtitle'} />
             <form onSubmit={e => e.preventDefault()}>
                 <div className="d-grid"
                     style={{
@@ -18,13 +26,16 @@ export default function MortgageCalculatorApp() {
                         gap: 10
                     }}>
 
-                    <FormGroup labelText={'Down payment'}
-                        inputType={'number'} placeholder={'Enter the value of the home'} />
                     <FormGroup labelText={'Home Value'}
-                        inputType={'number'} placeholder={'Enter your funds'} />
+                        inputType={'number'} placeholder={'Enter your funds'}
+                        values={homeValue} />
+                    <FormGroup labelText={'Down payment'}
+                        inputType={'number'} placeholder={'Enter the value of the home'}
+                        values={downPayment} />
                 </div>
                 <FormGroup labelText={'Loan amount'}
-                    inputType={'number'} placeholder={'Calculated amount of loan'} />
+                    inputType={'number'} placeholder={'Calculated amount of loan'} values={loanAmount}
+                    readOnly={true} className={'bg-light'} />
                 <div className="d-grid"
                     style={{
                         gridTemplateColumns: '1fr 1fr',
@@ -32,13 +43,16 @@ export default function MortgageCalculatorApp() {
                     }}>
 
                     <FormGroup labelText={'Interest Rate'}
-                        inputType={'number'} placeholder={'Enter the interest rate'} />
+                        inputType={'number'} placeholder={'Enter the interest rate'} values={interestRate} />
                     <FormGroup labelText={'Loan Duration (years)'}
-                        inputType={'number'} placeholder={'The Duration of loan in years'} />
+                        inputType={'number'} placeholder={'The Duration of loan in years'} values={loanDuration} />
                 </div>
                 <Button text='Calculate' btnClass={'btn-info btn-block'} />
-                <h4 className={`${(alertClass = 'alert-danger')}`}
-                    style={{ width: 'auto', margin: '1rem 0' }}></h4>
+                {monthlyPayment && (
+                    <h4 className={`${(alertClass = 'alert-danger')}`}
+                        style={{ width: 'auto', margin: '1rem 0' }}>
+                    </h4>
+                )}
             </form>
         </div>
     )
