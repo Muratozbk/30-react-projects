4055e76c252dbb63be0f69e68319b5c9ff2e7926
muratozbk <slipstream11@yandex.com>
2023-09-17 11:19:06 +0300

    posts load

--- src/projects/30-lazy-loading/LazyLoadingApp.jsx
+++ src/projects/30-lazy-loading/LazyLoadingApp.jsx
@@ -3,23 +3,34 @@ import LazyLoadPost from './LazyLoadPost';
 import Title from "../components/Title"
 
 export default function LazyLoadingApp() {
-
-    const [post, setPost] = useState([]);
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/photos/")
-            .then((response) => response.json())
-            .then((json) => setPost(json))
-    })
+        fetch("https://jsonplaceholder.typicode.com/photos/").then(response => response.json())
+            .then((json) => setPosts(json))
+    }, [])
     return (
-        <div className='container'>
-            <Title classes={"text-center mt-2"} text={"Posts Images"} />
-            <div className='container d-flex 
-            flex-column' style={{ gap: 30 }}>
-                {post.map(post => (<LazyLoadPost key={post.id} title={post.title}
-                    img={post.url} />
-                ))}
+        <div className="container">
+            <h1 className="text-center">Posts images</h1>
+            <div className="container d-flex flex-column"
+                style={{ gap: 30 }}>
+                {posts.map(post => {
+                    <LazyLoadPost key={post.id}
+                        title={post.title} img={post.url} />
+                })}
             </div>
         </div>
     )
 }
+
+// return (
+//     <div className='container'>
+//         <Title classes={"text-center mt-2"} text={"Posts Images"} />
+//         <div className='container d-flex
+//         flex-column' style={{ gap: 30 }}>
+//             {post.map(post => (<LazyLoadPost key={post.id} title={post.title}
+//                 img={post.url} />
+//             ))}
+//         </div>
+//     </div>
+// )
